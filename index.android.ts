205294ea36e0610b99bce93d066b9ddfed36d102
Ric <ricpmonteiro@gmail.com>
2018-12-07 22:31:52 +0100

    Remove useless file. Go away, demon

--- index.android.ts
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native'
-import { App } from './src/app'
-import widgetTask from './src/widgetTask'
-
-AppRegistry.registerHeadlessTask('widgetTask', () => widgetTask)
-AppRegistry.registerComponent('androidWidgetPoc', () => App)
